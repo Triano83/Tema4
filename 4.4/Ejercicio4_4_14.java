bfbe17377cf7910891c12fe13ccea9227629f277
Triano83 <135257725+Triano83@users.noreply.github.com>
2023-12-05 18:52:06 +0100

    UpdateEjercicio4_4_14.java

--- 4.4/Ejercicio4_4_14.java
+++ 4.4/Ejercicio4_4_14.java
@@ -8,13 +8,25 @@
 
 public class Ejercicio4_4_14 {
     public static void main(String[] args) {
-        int suma = 0;
 
+        int suma = 0;
+        // averiguamos el tamaño del array
         for (int i = 10; i > 0; i--) {
             suma = suma + i;
         }
-
+        // definimos el array con su tamaño correspondiente
         int[] array = new int[suma];
-
+        int contador = 0;
+        // 2 bucles para ir introduciendo los valores en el indice indicado
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                array[contador] = i;
+                contador++;
+            }
+        }
+        // mostramos los resultados
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
