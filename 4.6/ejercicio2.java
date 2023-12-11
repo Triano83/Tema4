2f379d14a45ec21ef88c5e841508d6b27bf7c978
Triano83 <135257725+Triano83@users.noreply.github.com>
2023-12-11 19:30:04 +0100

    ejercicio2.java

--- 4.6/ejercicio2.java
+++ 4.6/ejercicio2.java
@@ -8,16 +8,19 @@
 
 public class ejercicio2 {
     public static void main(String[] args) {
+
+//Declaracion de variables
         int fila;
         int columna;
         int[][] multiplicar = new int[10][10];
-
+        
+// Introduzco los resultados de las tablas de multiplicar dentro del array
         for (fila = 0; fila < 10; fila++) {
             for (columna = 0; columna < 10; columna++) {
                 multiplicar[fila][columna] = (fila + 1) * (columna + 1);
             }
         }
-
+// Uso los bucles for para ir imprimiendo
         for (fila = 0; fila < 10; fila++) {
             System.out.println();
             for (columna = 0; columna < 10; columna++) {
