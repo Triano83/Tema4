3e36a8866c75c04d1a8910c2669972c49769afaa
Triano83 <135257725+Triano83@users.noreply.github.com>
2023-12-11 19:13:16 +0100

    ejercicio1.java

--- 4.6/ejercicio1.java
+++ 4.6/ejercicio1.java
@@ -6,21 +6,21 @@
  */
 
 public class ejercicio1 {
-
+//Declaracion de variables y array
     public static void main(String[] args) {
         int[][] num = new int[5][5];
 
         int fila;
         int columna;
         int contador = 1;
-
+//Introducimos  los numeros en el array
         for (fila = 0; fila < 5; fila++) {
             for (columna = 0; columna < 5; columna++) {
                 num[fila][columna] = contador;
                 contador++;
             }
         }
-
+// Imprimimos el array dandole formato
         for (fila = 0; fila < 5; fila++) {
             System.out.println();
             for (columna = 0; columna < 5; columna++) {
