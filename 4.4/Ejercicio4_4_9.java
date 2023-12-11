74d0535c3023e6a779862ecf8ad4e942c7c73ae0
Triano83 <135257725+Triano83@users.noreply.github.com>
2023-12-11 13:31:57 +0100

    Ejercicio4_4_9.java

--- 4.4/Ejercicio4_4_9.java
+++ 4.4/Ejercicio4_4_9.java
@@ -1,14 +1,13 @@
 /**
- * Hacer un array de 100 espacios he introducir 100 numeros aleatorios luego
- * pedirá por teclado un numero y dira cuantas coincidencias existen en el
- * array.
+ * Crea un programa que cree un array de enteros de tamaño 100 y lo rellene con
+ * valores enteros aleatorios entre 1 y 10 (utiliza 1 + Math.random()*10). Luego
+ * pedirá un valor N y mostrará en qué posiciones del array aparece N.
  * 
  * @author Jose Luis Triano Pavón
  */
 public class Ejercicio4_4_9 {
     public static void main(String[] args) {
 
-        int contador = 0;
         boolean esVerdadero = true;
         int num = 0;
         int[] array100 = new int[100];
@@ -16,7 +15,6 @@ public class Ejercicio4_4_9 {
         for (int i = 0; i < 100; i++) {
             int random = (int) (Math.random() * 10) + 1;
             array100[i] = random;
-            System.out.println(random);
         }
 
         System.out.println("Introduce un numero del 1 al 100");
@@ -33,10 +31,8 @@ public class Ejercicio4_4_9 {
 
         for (int i = 0; i < 100; i++) {
             if (num == array100[i]) {
-                contador++;
+                System.out.println("Tu numero coincide en la posicion: " + i);
             }
         }
-        String vezVeces = contador == 1 ? " vez" : " veces";
-        System.out.println("El numero se ha repetido dentro del array: " + contador + vezVeces);
     }
 }
